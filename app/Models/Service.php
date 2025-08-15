f9aaa074468ae3fdf7a3363aa59a6742edf24fef
AhmedDip <ahmedrb.dip@gmail.com>
2025-08-15 09:46:48 +0600

    Added necessary models and migrations for service and booking

---
+++ app/Models/Service.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'status'];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+}
